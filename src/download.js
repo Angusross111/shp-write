1d549dca6c8918de13a074936e15babca86b6f7f
Angus Ross <angus.ross@swarmfarm.com>
2021-09-20 11:31:59 +1000

    Modernized download function

--- src/download.js
+++ src/download.js
@@ -2,6 +2,12 @@ var zip = require("./zip");
 
 module.exports = function (gj, options) {
     zip(gj, options).then(function (content) {
-        location.href = "data:application/zip;base64," + content;
+        var element = document.createElement("a");
+        element.setAttribute("href", "data:application/zip;base64," + content);
+        element.setAttribute("download", "spray-map.zip");
+        element.style.display = "none";
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
     });
 };
